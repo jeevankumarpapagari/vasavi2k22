6842cf2f7c085d12564652ac21b6ff85462be0b0
Jeevan Kumar Paapagaari <jeevankumar922@gmail.com>
2022-05-09 21:34:53 +0530

    DSOE 6E: Design Circular Queue using array

--- Week - 6/WEEK-6E.C
+++ Week - 6/WEEK-6E.C
@@ -1,118 +1,66 @@
-//DSOE 6E: Design Circular Queue using array
-/*
-Input Format
-
-First line an integer N, size of Queue
-Second line an integer Q, number of operations on circular queue
-Next Q lines each line contain one operation each:1 X denotes insert X value to queue, 2 denotes delete the first element,3 denotes print first element, 4 denotes print last element,5 denotes check the queue is empty or not
-Constraints
-
-1 <= N < = 10 1 <= Q < = 100
-
-Output Format
-
-Every answer should be in new line according to the problem statement
-
-Sample Input 0
-
-5
-13
-1 10
-1 20
-1 30
-1 40
-1 50
-1 60
-2
-1 60
-1 70
-2
-3
-4
-5
-Sample Output 0
-
-Queue is Full
-Queue is Full
-30
-60
-0
-*/
-
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
-int f=-1,r=-1;
-int *q,qsize;
-void isempty()
+#include <stdbool.h>
+
+int *queue, queueSize, front = -1, rear = -1;
+
+void enQueue(int ele)
 {
-    if(f==-1)
-        printf("1\n");
-    else 
-        printf("0\n");
-}
-void enq(int x)
-{
-    if( (f==0 && r==qsize-1) || (f==r+1) )
+    if((front==0 && rear==queueSize-1) || front==rear+1)
     {
         printf("Queue is Full\n");
         return;
     }
-    if(f==-1)
-        f=r=0;
-    else 
-        r=(r+1)%qsize;
-    q[r]=x;
-}
-void deq()
-{
-    if(f==-1)
-        printf("Queue is Empty\n");
-    else if(f==r)
-        f=r=-1;
-    else 
-        f=(f+1)%qsize;
-}
-void front()
-{
-    if(f==-1)
-        printf("Queue is Empty\n");
-    else 
-        printf("%d\n",q[f]);
-}
-void rear()
-{
-    if(f==-1)
-        printf("Queue is Empty\n");
-    else 
-        printf("%d\n",q[r]);
+    if(front==-1)
+        front = rear = 0;
+    else
+        rear = (rear+1)%queueSize;
+    queue[rear] = ele;
 }
 
-int main() 
+bool isEmpty()
 {
-    scanf("%d",&qsize);
-    q=(int*)malloc(sizeof(int)*qsize);
-    int t,ch,x;
-    scanf("%d",&t);
-    while(t--)
+    return front == -1;
+}
+
+void deQueue()
+{
+    if(isEmpty())
+        printf("Queue is Empty\n");
+    else if(front==rear)
+        front = rear = -1;
+    else
+        front = (front+1)%queueSize;
+}
+
+void frontElement()
+{
+    isEmpty() ? printf("Queue is Empty\n") : printf("%d\n",queue[front]);
+}
+
+void rearElement()
+{
+    isEmpty() ? printf("Queue is Empty\n") : printf("%d\n",queue[rear]);
+}
+
+int main()
+{
+    scanf("%d",&queueSize);
+    queue = (int*) malloc(sizeof(int) * queueSize);
+    int n;
+    scanf("%d",&n);
+    while(n--)
     {
-        scanf("%d",&ch);
-        if(ch==1)
+        int choice, ele;
+        scanf("%d",&choice);
+        switch(choice)
         {
-           scanf("%d",&x);
-            enq(x);
+            case 1: scanf("%d",&ele); enQueue(ele); break;
+            case 2: deQueue(); break;
+            case 3: frontElement(); break;
+            case 4: rearElement(); break;
+            case 5: isEmpty() ? printf("1\n") : printf("0\n"); break;
         }
-        else if(ch==2)
-            deq();
-        else if(ch==3)
-            front();
-        else if(ch==4)
-            rear();
-        else 
-            isempty();
     }
-   
     return 0;
 }
-
