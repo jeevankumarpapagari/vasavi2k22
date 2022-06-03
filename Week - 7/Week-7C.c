9bb2f2cd5b5bc65b1c1805f836b0f21e4ebdd857
Jeevan Kumar Paapagaari <jeevankumar922@gmail.com>
2022-06-03 12:31:50 +0530

    DSOE 7C: Arrange the given Array-2

---
+++ Week - 7/Week-7C.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int arr[n], i;
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    i = n-2;
+    int key = arr[n-1];
+    while(i>=0 && arr[i]>key)
+    {
+        if(arr[i] > key)
+            arr[i+1] = arr[i];
+        i--;
+    }
+    arr[i+1] = key;
+
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+
+    return 0;
+}
