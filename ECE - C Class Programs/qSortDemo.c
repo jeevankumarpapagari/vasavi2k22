d7eedf3af06d032cb8fcff24de9db1a9773aa99c
Jeevan Kumar Paapagaari <jeevankumar922@gmail.com>
2022-06-07 10:45:56 +0530

    qSort Demo

---
+++ ECE - C Class Programs/qSortDemo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const void *x,const void *y)
+{
+	//return *(int*)x - *(int*)y;//Ascending order
+	return *(int*)y - *(int*)x;//Descending order
+}
+int main()
+{
+	int arr[9] = {1,5,9,3,7,6,2,4,8}, i;
+	qsort(arr,9,sizeof(int),compare);
+	for(i=0;i<9;i++)
+		printf("%d ",arr[i]);
+}
